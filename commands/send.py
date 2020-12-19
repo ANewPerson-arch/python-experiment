9be3000039e984f54c863dcddfc1268a2a15ee13
ANewPerson-arch <70480483+ANewPerson-arch@users.noreply.github.com>
2020-12-19 15:26:14 -0500

    Update send.py

--- commands/send.py
+++ commands/send.py
@@ -1,3 +1,2 @@
-class Send:
-     async def execute(message, args):
-        await message.channel.send('Nerds usin module.exports')
+async def execute(message, args):
+      await message.channel.send('Nerds usin module.exports')
